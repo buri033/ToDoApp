96410ef7740c7f7252553fefc7991917585b8679
Tomás Buriticá Jaramillo <tomasburitica17@gmail.com>
2024-08-22 10:38:47 -0500

    Create methods mark_completed and add_tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,8 +1,18 @@
 class Todo:
-    def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]):
+    def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str], tag: str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
+        self.tag: str = tag
 
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self):
+
+        if self.tag in self.tags:
+            return self.tags
+        else:
+            self.tags.append(self.tag)
