3f272c6a4c012aff14f689c34ca8cb6581747e13
Tomás Buriticá Jaramillo <tomasburitica17@gmail.com>
2024-08-22 11:39:50 -0500

    Fixed some problems, and add add_todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,18 +1,34 @@
+import random
+
+
 class Todo:
-    def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str], tag: str):
+    def __init__(self, code_id: int, title: str, description: str):
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags: list[str] = []
-        self.tag: str = tag
 
     def mark_completed(self):
-        self.completed = True
+        self.completed: bool = True
 
-    def add_tag(self):
+    def add_tag(self, tag: str):
 
-        if self.tag in self.tags:
+        if tag in self.tags:
             return self.tags
         else:
-            self.tags.append(self.tag)
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id}-{self.title}"
+
+
+class TodoBook:
+    def __init__(self):
+        self.todos: dict = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        id = len(self.todos) + 1
+        objecttodo = Todo(4, "Cien años de Recocha", "Camarón que se parcha, se lo lleva la recocha")
+        self.todos[4] = objecttodo
+        return id
